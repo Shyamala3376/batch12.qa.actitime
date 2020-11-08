5e1474df95b6145b6414a9e4350bdacca9a72f7d
shyam <shyam@LAPTOP-NMG86DD0>
2020-11-07 22:38:24 -0500

    Made changes TC01

--- src/main/java/com/acti/base/DriverScript.java
+++ src/main/java/com/acti/base/DriverScript.java
@@ -1,5 +1,87 @@
 package com.acti.base;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/* Name : Driver Script
+ * Description: XXXXXXXX
+ * Developed by : Shyamala
+ * Date: 11/7/20
+ */
+
+
 public class DriverScript {
+	
+	public static WebDriver driver;
+	static Properties prop;
+	//Create a constructor for getting the file 
+	public DriverScript()
+	{
+		try
+		{
+			File file = new File("./src/test/resources/configfiles/config.properties");
+			FileInputStream FIS = new FileInputStream(file);
+			prop = new Properties();
+			prop.load(FIS);
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Unable to read the file" + e.getMessage());    
+		}
+	}	
+		
+		//Create a method to open the browser
+	
+		public static void  initBrowser()
+		{
+			String browser = prop.getProperty("browser");
+			if (browser.equalsIgnoreCase("chrome"))
+			{
+				System.setProperty("webdriver.chrome.driver","./src/test/resources/browsers/chromedriver.exe");
+				driver = new ChromeDriver();
+			}
+			else if(browser.equalsIgnoreCase("Firefox"))
+			{
+				System.setProperty("webdriver.gecko.driver","./src/test/resources/browsers/geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+			else
+			{
+				System.out.println("Unsupported Browser Name");			
+			}
+			driver.manage().window().maximize();
+			driver.manage().deleteAllCookies();
+			driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			LaunchApplication();
+			try
+			{
+				Thread.sleep(3000);
+			}
+			catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+			// CloseApplication();
+		}
+
+     public static void LaunchApplication()
+
+    {
+	    String url = prop.getProperty("qaurl");
+        driver.get(url);
+     }
+     
+     public static void CloseApplication()
+     {
+    	 driver.quit();
+     }
 
 }
--- src/main/java/com/acti/pages/loginpage.java
+++ src/main/java/com/acti/pages/loginpage.java
@@ -1,7 +1,57 @@
 package com.acti.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.acti.base.DriverScript;
 
 public class loginpage extends DriverScript {
+
+	/* Name : LoginPage
+	 * Description: Elements and Actions 
+	 * Developed by : Shyamala
+	 * Date: 11/7/20
+	 */
+	
+	
+	//*******************************************Page Element *************************************************//
+	@FindBy(id="username") WebElement usernameTB;
+	@FindBy(name="pwd") WebElement passwordTB;
+	@FindBy(id="loginButton")WebElement LoginButton;
+	@FindBy(className="atLogoImg") WebElement actitimeLogo; 
+	
+	//*******************************************Page Initialization *******************************************//
+	
+	public loginpage()
+	{
+		PageFactory.initElements(driver,this);
+	}
+	
+
+	//*******************************************Page Actions****************************************************//
+	/* You should never hard code the test data. so it has to be parameterised.
+	public void logintoApplication()
+	{
+		usernameTB.sendKeys("admin");
+		passwordTB.sendKeys("manager");
+		LoginButton.click();
+	}*/
+	
+	public void logintoApplication(String username,String password)
+	{
+		usernameTB.sendKeys(username);
+		passwordTB.sendKeys(password);
+		LoginButton.click();
+	}
+	public boolean verifylogo()
+	{
+		return actitimeLogo.isDisplayed();
+	}
+	public String verifypagetitle()
+	{
+	    return driver.getTitle();
+	}
+	
 
 }
